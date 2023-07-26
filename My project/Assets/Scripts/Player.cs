0a599285a952ccf2f08f9617c8fc8ce4a3c4e543
LucasNeves96 <lucasvalente.ng@gmail.com>
2023-07-26 17:12:54 -0300

    Testando

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PLayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Input.getkey
     }
 }
