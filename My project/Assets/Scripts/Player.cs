ed8cfcb4d25f7acd3d5dced8a7ecd2f069002984
LucasNeves96 <lucasvalente.ng@gmail.com>
2023-07-31 17:54:34 -0300

    Clear Counter Implemented

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -7,12 +7,48 @@ public class Player : MonoBehaviour
    [SerializeField] float MoveSpeed = 7;
     //SerializeField significa q ele vai aparecer no nosso menu da Unity.
    [SerializeField] GameInput gameInput;
+   [SerializeField] LayerMask layerMask;
    private bool isWalking;
-   private Vector2 InputVector;
+   private Vector3 LastInteractDir;
 
    private void Update()
    {
-      InputVector = gameInput.GetMovementVectorNormalized();
+      HandleWalking();
+      HandleInteractions();
+   }
+
+   public bool IsWalking()
+   {
+      return isWalking;
+   }
+
+   private void HandleInteractions()
+   {
+      Vector2 InputVector = gameInput.GetMovementVectorNormalized();
+      Vector3 MoveDir = new Vector3(InputVector.x, 0f, InputVector.y);
+
+      if (MoveDir != Vector3.zero)
+      {
+         LastInteractDir = MoveDir;
+      }
+
+      float InteractDistance = 2f;
+      if(Physics.Raycast(transform.position, LastInteractDir, out RaycastHit raycastHit, InteractDistance, layerMask))
+      {
+         if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+         {
+            // Has Clear Counter!
+            clearCounter.Interact(); 
+         }
+      }
+      // raycastHit retorna o objeto q foi alvejado (e é um 'out' que significa que ele retorna esse valor da função).
+
+   }
+
+
+   private void HandleWalking()
+   {
+      Vector2 InputVector = gameInput.GetMovementVectorNormalized();
       Vector3 MoveDir = new Vector3(InputVector.x, 0f, InputVector.y);
       
       float MoveDistance = MoveSpeed * Time.deltaTime;
@@ -52,12 +88,6 @@ public class Player : MonoBehaviour
       transform.forward = Vector3.Slerp(transform.forward, MoveDir, Time.deltaTime * RotationSpeed);
 
       isWalking = (Vector3)InputVector != Vector3.zero;
-
-      Debug.Log(InputVector);
    }
 
-   public bool IsWalking()
-   {
-      return isWalking;
-   }
 }
