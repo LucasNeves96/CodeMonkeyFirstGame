25f7bd47f50928b5da23d1b7a929f4aea84b9d37
LucasNeves96 <lucasvalente.ng@gmail.com>
2023-08-04 20:37:10 -0300

    Spawn de objetos e swap deles

--- My project/Assets/Scripts/ClearCounter.cs
+++ My project/Assets/Scripts/ClearCounter.cs
@@ -4,10 +4,63 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-   
+   [SerializeField] private KitchenObjectsSO KitchenObjectSO;
+   [SerializeField] private Transform  CounterTopPoint;
+   [SerializeField] private ClearCounter SecondClearCounter;
+   [SerializeField] private bool testing;
+
+   private KitchenObject KitchenObject = null;
+
+   private void Update() {
+      if(testing && Input.GetKeyDown(KeyCode.T))
+      {
+         if(KitchenObject != null)
+         {
+            KitchenObject.SetClearCounter(SecondClearCounter);
+         }
+      }
+   }
+
    public void Interact()  
    {
-        Debug.Log("Interacting!");
+      if(KitchenObject == null)
+      {
+        Transform KitchenObjectTransform = Instantiate(KitchenObjectSO.Prefab, CounterTopPoint);
+        KitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this); 
+        KitchenObjectTransform.transform.localPosition = Vector3.zero;
+
+        KitchenObject = KitchenObjectTransform.GetComponent<KitchenObject>(); 
+
+        KitchenObject.SetClearCounter(this);
+      }
+      else
+      {
+         Debug.Log(KitchenObject.GetClearCounter());
+      }
+   }
+
+   public Transform GetKitchenObjectFollowTransform()
+   {
+      return CounterTopPoint;
    }
     
+   public void SetKitchenObject(KitchenObject kitchenObject)
+   {
+      this.KitchenObject = kitchenObject;
+   }
+
+   public KitchenObject GetKitchenObject()
+   {
+      return this.KitchenObject;
+   }
+
+   public void ClearKitchenObject()
+   {
+      this.KitchenObject = null;
+   }
+
+   public bool HasKitchenObject()
+   {
+      return KitchenObject != null;
+   }
 }
---
+++ My project/Assets/Scripts/KitchenObjectsSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class KitchenObjectsSO : ScriptableObject
+{
+    public Transform Prefab;
+    public Sprite Sprite;
+    public string ObjectName;
+
+
+}
+ 
